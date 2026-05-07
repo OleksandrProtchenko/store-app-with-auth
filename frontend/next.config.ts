8258bf41efe62041ea533f6689116014b0105658
Oleksandr Protchenko <alexdevelop92@gmail.com>
2026-05-07 18:33:16 +0300

    fix path in next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,13 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // experimental: {
-  //   staleTimes: {
-  //     dynamic: 0,
-  //     static: 180,
-  //   },
-  // },
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.BACKEND_URL}/auth/:path*`,
+      },
+      {
+        source: "/api/orders/:path*",
+        destination: `${process.env.BACKEND_URL}/orders/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
